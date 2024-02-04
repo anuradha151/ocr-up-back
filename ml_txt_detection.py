0a7fbb9ab71792e4760a5baf3b208f1ec78831df
Anuradha Ranasinghe <anuranasinghe151@gmail.com>
2024-02-04 16:44:55 +0530

    Feature : add dynamic image uploading

--- ml_txt_detection.py
+++ ml_txt_detection.py
@@ -3,12 +3,12 @@ import requests
 from PIL import Image
 
 def detect(file):
+
+    image = Image.open(file).convert("RGB")
+
     processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-handwritten")
     model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-base-handwritten")
-
-    url = "https://d2gi74td4gf3sj.cloudfront.net/games/7b70ace6-bd74-4abf-b710-98978d2560f9/assets/title_image/a01-122-02.jpg"
-    image = Image.open(requests.get(url, stream=True).raw).convert("RGB")
-
+    
     pixel_values = processor(image, return_tensors="pt").pixel_values
     generated_ids = model.generate(pixel_values)
 
